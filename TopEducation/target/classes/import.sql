da64f70d632a4d3206d1cda179ef3929b9032ad8
AracelyC1 <aracely.castro@usach.cl>
2023-10-05 18:36:40 -0300

    introduciendo jenkins

---
+++ TopEducation/target/classes/import.sql
@@ -0,0 +1,11 @@
+
+use topedu;
+
+INSERT INTO estudiante (rut, nombre_estudiante, apellido_estudiante, fecha_nacimiento, tipo_escuela, nombre_escuela, anio_egreso)
+VALUES ('123456789', 'Juan', 'Pérez', '2002/08/16', 'municipal', 'Escuela 1', '2023');
+
+INSERT INTO estudiante (rut, nombre_estudiante, apellido_estudiante, fecha_nacimiento, tipo_escuela, nombre_escuela, anio_egreso)
+VALUES ('987654321', 'María', 'González', '2000/05/23', 'subvencionado', 'Escuela 2', '2022');
+
+INSERT INTO estudiante (rut, nombre_estudiante, apellido_estudiante, fecha_nacimiento, tipo_escuela, nombre_escuela, anio_egreso)
+VALUES ('456789123', 'Carlos', 'Martínez', '2001/03/12','privado', 'Escuela 3', '2022');
