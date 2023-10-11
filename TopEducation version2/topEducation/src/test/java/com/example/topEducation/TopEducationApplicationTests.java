784568e652e3362c78bea4372f41d5cca294e314
AracelyC1 <aracely.castro@usach.cl>
2023-10-11 19:42:40 -0300

    versión 2 de topEducation
    debido a un error en el primer proyecto que no tengo forma de arreglar es que muevo todo mi desarrollo a este proyecto

---
+++ TopEducation version2/topEducation/src/test/java/com/example/topEducation/TopEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.topEducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
