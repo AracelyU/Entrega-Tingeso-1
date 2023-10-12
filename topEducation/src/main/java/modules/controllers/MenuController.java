588dfcfcdba4bc61bf75f8b79d7e2bd3d227ea8f
AracelyC1 <aracely.castro@usach.cl>
2023-10-11 22:50:05 -0300

    cambio la ruta menu y elimino una réplica

--- topEducation/src/main/java/modules/controllers/MenuController.java
+++ topEducation/src/main/java/modules/controllers/MenuController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MenuController {
 
-    @GetMapping("/menu")
+    @GetMapping("/")
     public String main(){
-        return "/menu";
+        return "menu";
     }
 
 }
